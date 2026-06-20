e1a2d981d5576c287bae34c52922df7fc67e24a0
aisreedhar3d <aisreedhar3d@gmail.com>
2026-06-20 16:35:06 +0530

    Add files via upload

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-10",
-      "name": "10.",
+      "id": "0-chepalpuda-vr-renders_2---panorama-copy",
+      "name": "Chepalpuda vr renders_2 - Panorama copy",
       "levels": [
         {
           "tileSize": 256,
@@ -20,23 +20,30 @@ var APP_DATA = {
       ],
       "faceSize": 1024,
       "initialViewParameters": {
-        "yaw": -0.4914477302184679,
-        "pitch": 0.047387236369100805,
-        "fov": 1.1166367482365231
+        "yaw": -1.1966942359355137,
+        "pitch": 0.15005910879397355,
+        "fov": 1.2861142494464142
       },
       "linkHotspots": [
         {
-          "yaw": -2.477199969402699,
-          "pitch": 0.000351957850586615,
+          "yaw": -1.3044248415414579,
+          "pitch": -0.0824781657721676,
           "rotation": 0,
-          "target": "1-2jpg"
+          "target": "1-chepalpuda-vr-renders_3---panorama-copy"
         }
       ],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": -1.9120964153989988,
+          "pitch": 0.05716967700881348,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
     },
     {
-      "id": "1-2jpg",
-      "name": "2.jpg",
+      "id": "1-chepalpuda-vr-renders_3---panorama-copy",
+      "name": "Chepalpuda vr renders_3 - Panorama copy",
       "levels": [
         {
           "tileSize": 256,
@@ -60,10 +67,210 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 3.1353368514466533,
-          "pitch": 0.06659504624053092,
+          "yaw": -0.1229397889498447,
+          "pitch": -0.10514346381572715,
           "rotation": 0,
-          "target": "0-10"
+          "target": "2-chepalpuda-vr-renders_4---panorama-copy"
+        },
+        {
+          "yaw": -2.7612481823534765,
+          "pitch": 0.13210285677612,
+          "rotation": 0,
+          "target": "0-chepalpuda-vr-renders_2---panorama-copy"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-chepalpuda-vr-renders_4---panorama-copy",
+      "name": "Chepalpuda vr renders_4 - Panorama copy",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.02661532098185937,
+          "pitch": 0.06371918137528709,
+          "rotation": 0,
+          "target": "3-chepalpuda-vr-renders_5---panorama-copy"
+        },
+        {
+          "yaw": 1.175460334652593,
+          "pitch": 0.14214678224184496,
+          "rotation": 0,
+          "target": "4-chepalpuda-vr-renders_6---panorama-copy"
+        },
+        {
+          "yaw": 3.125666657203986,
+          "pitch": 0.01228912720519304,
+          "rotation": 0,
+          "target": "1-chepalpuda-vr-renders_3---panorama-copy"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-chepalpuda-vr-renders_5---panorama-copy",
+      "name": "Chepalpuda vr renders_5 - Panorama copy",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.08155066012181322,
+          "pitch": 0.02867639055349258,
+          "rotation": 0,
+          "target": "2-chepalpuda-vr-renders_4---panorama-copy"
+        },
+        {
+          "yaw": 3.094145858614441,
+          "pitch": 0.0019287025552028325,
+          "rotation": 0,
+          "target": "2-chepalpuda-vr-renders_4---panorama-copy"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-chepalpuda-vr-renders_6---panorama-copy",
+      "name": "Chepalpuda vr renders_6 - Panorama copy",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1316417446361804,
+          "pitch": 0.08877774498850677,
+          "rotation": 0,
+          "target": "2-chepalpuda-vr-renders_4---panorama-copy"
+        },
+        {
+          "yaw": -1.335294880978477,
+          "pitch": 0.026904525150161618,
+          "rotation": 0,
+          "target": "6-chepalpuda-vr-renders_8---panorama-copy"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-chepalpuda-vr-renders_7---panorama-copy",
+      "name": "Chepalpuda vr renders_7 - Panorama copy",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.027250223349080116,
+          "pitch": 0.0007130524569234353,
+          "rotation": 0,
+          "target": "4-chepalpuda-vr-renders_6---panorama-copy"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-chepalpuda-vr-renders_8---panorama-copy",
+      "name": "Chepalpuda vr renders_8 - Panorama copy",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1389684497734915,
+          "pitch": 0.052603314447580374,
+          "rotation": 0,
+          "target": "5-chepalpuda-vr-renders_7---panorama-copy"
         }
       ],
       "infoHotspots": []
@@ -73,7 +280,7 @@ var APP_DATA = {
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
-    "fullscreenButton": false,
-    "viewControlButtons": false
+    "fullscreenButton": true,
+    "viewControlButtons": true
   }
 };
